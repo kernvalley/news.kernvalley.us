b01e9331fc2dad3be730c746788e14ff95d4a048
Chris Zuber <shgysk8zer0@gmail.com>
2020-11-26 10:08:32 -0800

    Misc. fixes
    - Fix setting various properties
    - Fix missing weather config & script

--- js/index.js
+++ js/index.js
@@ -11,6 +11,7 @@ import 'https://cdn.kernvalley.us/components/leaflet/map.js';
 import 'https://cdn.kernvalley.us/components/leaflet/marker.js';
 import 'https://cdn.kernvalley.us/components/pwa/install.js';
 import 'https://cdn.kernvalley.us/components/ad/block.js';
+import 'https://cdn.kernvalley.us/components/weather/current.js';
 import { $, ready } from 'https://cdn.kernvalley.us/js/std-js/functions.js';
 import { loadScript } from 'https://cdn.kernvalley.us/js/std-js/loader.js';
 import { importGa, externalHandler, telHandler, mailtoHandler } from 'https://cdn.kernvalley.us/js/std-js/google-analytics.js';
